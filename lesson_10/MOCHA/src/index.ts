a512484d404136a74ed9e603534f108c4e990a8a
Artur <arthurbalyakin@gmail.com>
2025-10-22 21:48:42 +0300

    lesson 10 fix

---
+++ lesson_10/MOCHA/src/index.ts
@@ -0,0 +1,31 @@
+import { IAircraft } from './abstruction/i-aircraft';
+import { Drone } from './drone';
+
+
+class Fly {
+    public constructor (public readonly name: string) {}
+
+    public takeOff(drone: IAircraft): void {
+        drone.powerOn();
+    }
+    public land(drone: IAircraft): void {
+        drone.powerOff();
+    }
+}
+
+const flyMode = new Fly('My drone');
+const drone = new Drone();
+
+drone.increaseThrotlle(10);
+flyMode.takeOff(drone);
+drone.decreaseThrotlle(10);
+flyMode.takeOff(drone);
+drone.increaseThrotlle(10);
+flyMode.land(drone);
+
+drone.switchVtxPower(2);
+
+drone.startRecording();
+drone.stopRecording();
+drone.stopRecording();
+
